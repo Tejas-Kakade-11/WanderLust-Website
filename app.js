428965d5d0e40afa74a4e1bb6e4dc2dd687cafbc
tejas kakade <tejaskakade118@gmail.com>
2024-07-11 08:51:44 +0530

    All Files

--- app.js
+++ app.js
@@ -54,7 +54,7 @@ app.use(express.static(path.join(__dirname ,"/public")))
 const store = MongoStore.create({
     mongoUrl:dbUrl,
     crypto:{
-        secret : "mysupersecretstring"
+        secret : "process.env.SECRET"
     },
     touchAfter: 24 * 3600,
     
@@ -66,7 +66,7 @@ store.on("error",()=>{
 
 const sessionOptions= {
     store,
-    secret: "mysupersecretstring",
+    secret: "process.env.SECRET",
     resave:false,
     saveUninitialized:true,
 
